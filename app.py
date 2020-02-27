1704af4ae3ad436bc5cb4205094d30c361b9ec5c
tigrohvost <30686682+tigrohvost@users.noreply.github.com>
2020-02-27 16:51:09 +0300

    Update app.py

--- app.py
+++ app.py
@@ -24,30 +24,44 @@ app.layout = html.Div([
     html.H1('Welcome to the app'),
     html.H3('You are successfully authorized'),
     html.H3(some_var),
-    dcc.Dropdown(
-        id='dropdown',
-        options=[{'label': i, 'value': i} for i in ['A', 'B']],
-        value='A'
-    ),
-    dcc.Graph(id='graph')
+    dcc.Graph(
+        figure=dict(
+            data=[
+                dict(
+                    x=[1995, 1996, 1997, 1998, 1999, 2000, 2001, 2002, 2003,
+                    2004, 2005, 2006, 2007, 2008, 2009, 2010, 2011, 2012],
+                    y=[219, 146, 112, 127, 124, 180, 236, 207, 236, 263,
+                    350, 430, 474, 526, 488, 537, 500, 439],
+                    name='Rest of world',
+                    marker=dict(
+                        color='rgb(55, 83, 109)'
+                    )
+                ),
+                dict(
+                    x=[1995, 1996, 1997, 1998, 1999, 2000, 2001, 2002, 2003,
+                    2004, 2005, 2006, 2007, 2008, 2009, 2010, 2011, 2012],
+                    y=[16, 13, 10, 11, 28, 37, 43, 55, 56, 88, 105, 156, 270,
+                    299, 340, 403, 549, 499],
+                    name='China',
+                    marker=dict(
+                        color='rgb(26, 118, 255)'
+                    )
+                )
+            ],
+            layout=dict(
+                title='US Export of Plastic Scrap',
+                showlegend=True,
+                legend=dict(
+                    x=0,
+                    y=1.0
+                ),
+                margin=dict(l=40, r=0, t=40, b=30)
+            )
+        ),
+        style={'height': 300},
+        id='my-graph'
+    )  
 ], className='container')
-
-@app.callback(
-    dash.dependencies.Output('graph', 'figure'),
-    [dash.dependencies.Input('dropdown', 'value')])
-def update_graph(dropdown_value):
-    return {
-        'layout': {
-            'title': 'Graph of {}'.format(dropdown_value),
-            'margin': {
-                'l': 20,
-                'b': 20,
-                'r': 10,
-                't': 60
-            }
-        },
-        'data': [{'x': [1, 2, 3], 'y': [4, 1, 2]}]
-    }
 
 if __name__ == '__main__':
     app.run_server(debug=True)
