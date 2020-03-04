815b437ca59e8945e4f91057900c0df7e0fa0f8c
tigrohvost <30686682+tigrohvost@users.noreply.github.com>
2020-03-04 16:33:00 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,67 +1,61 @@
 import dash
-import dash_auth
+from dash.dependencies import Output, Input
 import dash_core_components as dcc
 import dash_html_components as html
 import plotly
+import plotly.graph_objs as go
+from collections import deque
 
-# Keep this out of source code repository - save in a file or a database
-VALID_USERNAME_PASSWORD_PAIRS = {
-    'hello': 'world'
-}
 
-external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
-
-app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
-server = app.server
-auth = dash_auth.BasicAuth(
-    app,
-    VALID_USERNAME_PASSWORD_PAIRS
+X = deque(maxlen=20)
+X.append(1)
+Y = deque(maxlen=20)
+Y.append(1)
+initial_trace = plotly.graph_objs.Scatter(
+    x=list(X),
+    y=list(Y),
+    name='Scatter',
+    mode='lines+markers'
 )
 
-some_var = '^_^'
-
-app.layout = html.Div([
-    html.H1('Welcome to the app'),
-    html.H3('You are successfully authorized'),
-    html.H3(some_var),
-    dcc.Graph(
-        figure=dict(
-            data=[
-                dict(
-                    x=[1995, 1996, 1997, 1998, 1999, 2000, 2001, 2002, 2003,
-                    2004, 2005, 2006, 2007, 2008, 2009, 2010, 2011, 2012],
-                    y=[219, 146, 112, 127, 124, 180, 236, 207, 236, 263,
-                    350, 430, 474, 526, 488, 537, 500, 439],
-                    name='Rest of world',
-                    marker=dict(
-                        color='rgb(55, 83, 109)'
-                    )
-                ),
-                dict(
-                    x=[1995, 1996, 1997, 1998, 1999, 2000, 2001, 2002, 2003,
-                    2004, 2005, 2006, 2007, 2008, 2009, 2010, 2011, 2012],
-                    y=[16, 13, 10, 11, 28, 37, 43, 55, 56, 88, 105, 156, 270,
-                    299, 340, 403, 549, 499],
-                    name='China',
-                    marker=dict(
-                        color='rgb(26, 118, 255)'
-                    )
-                )
-            ],
-            layout=dict(
-                title='US Export of Plastic Scrap',
-                showlegend=True,
-                legend=dict(
-                    x=0,
-                    y=1.0
-                ),
-                margin=dict(l=40, r=0, t=40, b=30)
-            )
+app = dash.Dash(__name__)
+server = app.server()
+app.layout = html.Div(
+    [
+        dcc.Graph(id='live-graph',
+                  animate=True,
+                  figure={'data': [initial_trace],
+                          'layout': go.Layout(
+                              xaxis=dict(range=[min(X), max(X)]),
+                              yaxis=dict(range=[min(Y), max(Y)]))
+                          }),
+        dcc.Interval(
+            id='graph-update',
+            interval=1*1000
         ),
-        style={'height': 300},
-        id='my-graph'
-    )  
-], className='container')
+    ]
+)
+
+
+@app.callback(Output('live-graph', 'figure'),
+              [Input('graph-update', 'n_intervals')])
+def update_graph_scatter(n):
+    X.append(X[-1]+1)
+    Y.append(Y[-1]+2)
+
+    trace = plotly.graph_objs.Scatter(
+        x=list(X),
+        y=list(Y),
+        name='Scatter',
+        mode='lines+markers'
+    )
+
+    return {'data': [trace],
+            'layout': go.Layout(
+                xaxis=dict(range=[min(X), max(X)]),
+                yaxis=dict(range=[min(Y), max(Y)]))
+            }
+
 
 if __name__ == '__main__':
     app.run_server(debug=True)
